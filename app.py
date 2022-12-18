2ce6ce044172f14d9a54a328477117c5471c3472
Darrion Burgess <dargondab9@gmail.com>
2022-12-18 10:26:27 -0800

    working on some stuff

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
+import pandas as dataframes
+
+
 def create_example_dataframe():
-    return None
+    return dataframes.DataFrame(
+        {
+            "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+            "Amount": [4, 1, 2, 2, 4, 5],
+            "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"],
+        }
+    )
--- test_app.py
+++ test_app.py
@@ -1,6 +1,13 @@
 import app
+
+
 def test_example_dataframe_creation():
     test_dataframe = app.create_example_dataframe()
 
-    assert test_dataframe is not None
-
+    assert test_dataframe is app.dataframes.DataFrame(
+        {
+            "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+            "Amount": [4, 1, 2, 2, 4, 5],
+            "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"],
+        }
+    )
