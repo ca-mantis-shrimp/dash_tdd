61e611921eabb5a300c352dd245330775a215b22
Darrion Burgess <dargondab9@gmail.com>
2022-12-18 10:36:57 -0800

    got the first test working

--- test_app.py
+++ test_app.py
@@ -1,13 +1,25 @@
 import app
 
+import pandas as dataframes
+
 
 def test_example_dataframe_creation():
     test_dataframe = app.create_example_dataframe()
 
-    assert test_dataframe is app.dataframes.DataFrame(
-        {
-            "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-            "Amount": [4, 1, 2, 2, 4, 5],
-            "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"],
-        }
+    app.dataframes.testing.assert_frame_equal(
+        test_dataframe,
+        dataframes.DataFrame(
+            {
+                "Fruit": [
+                    "Apples",
+                    "Oranges",
+                    "Bananas",
+                    "Apples",
+                    "Oranges",
+                    "Bananas",
+                ],
+                "Amount": [4, 1, 2, 2, 4, 5],
+                "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"],
+            }
+        ),
     )
