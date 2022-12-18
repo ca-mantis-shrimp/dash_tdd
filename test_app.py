7d5f48a2bac681b1169cb4d31fac5ce9013488b9
Darrion Burgess <dargondab9@gmail.com>
2022-12-18 12:01:24 -0800

    got some good tests so far

--- test_app.py
+++ test_app.py
@@ -39,4 +39,7 @@ def test_example_bar_chart():
 
     test_bar_chart = app.create_example_barchart(test_dataframe)
 
-    assert test_bar_chart.layout["barmode"] == "group"
+    assert test_bar_chart.layout.barmode == "group"
+    assert test_bar_chart.layout.xaxis.title.text == "Fruit"
+    assert test_bar_chart.layout.yaxis.title.text == "Amount"
+    assert test_bar_chart.layout.legend.title.text == "City"
